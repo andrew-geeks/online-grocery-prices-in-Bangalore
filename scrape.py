af2897bbb3c702b75abcfaec3dfb236abc812fee
Andrew George Issac <andrewgeorgeissac@gmail.com>
2023-07-29 11:58:26 +0530

    added json-updated functions

--- scrape.py
+++ scrape.py
@@ -62,67 +62,10 @@ for i in j_data["data"]:
     keys = i.keys()
     values = i.values()
     for j in keys:
-        getData(j,i[j])
+        try:
+            getData(j,i[j])
+        except:
+            pass
+        
        
 driver.close()      
-    
-
-
-
-
-
-
-# driver.get("https://www.bigbasket.com/pd/10000159/fresho-potato-1-kg/")
-# value=driver.find_element(By.CLASS_NAME, 'IyLvo')
-# value=value.text
-# value1=driver.find_element(By.CLASS_NAME, '_2ifWF')
-# value1=value1.text
-# value2=driver.find_element(By.XPATH,'//*[@id="10000159"]/tr[3]/td[2]')
-# amt_discounted_prcntge=value2.text
-# MRP=value1.split(' ')
-# MRP=MRP[1] #mrp
-
-# actualPrice=value.split(' ')
-# actualPrice=actualPrice[1] #price
-# dateval=str(datetime.date.today()) 
-# date=dateval.split('-')
-# date=date[2]+'-'+date[1]+'-'+date[0] #date
-# now = datetime.datetime.now()
-# month = now.strftime("%B") #month
-
-# #csv
-# data=[[date,month,MRP,actualPrice,amt_discounted_prcntge]]
-# with open('datasets/potato1kgprice_dataset.csv','a',newline='') as file:
-#             writer=csv.writer(file)
-#             writer.writerows(data)
-
-# ###########################################################################
-
-# driver.get("https://www.bigbasket.com/pd/10000148/fresho-onion-1-kg/")
-# value=driver.find_element(By.CLASS_NAME, 'IyLvo')
-# value=value.text
-# value1=driver.find_element(By.CLASS_NAME, '_2ifWF')
-# value1=value1.text
-# value2=driver.find_element(By.XPATH,'//*[@id="10000148"]/tr[3]/td[2]')
-# amt_discounted_prcntge=value2.text
-# MRP=value1.split(' ')
-# MRP=MRP[1] #mrp
-
-# actualPrice=value.split(' ')
-# actualPrice=actualPrice[1] #price
-# dateval=str(datetime.date.today()) 
-# date=dateval.split('-')
-# date=date[2]+'-'+date[1]+'-'+date[0] #date
-# now = datetime.datetime.now()
-# month = now.strftime("%B") #month
-
-# #csv
-# data=[[date,month,MRP,actualPrice,amt_discounted_prcntge]]
-# with open('datasets/onion1kgprice_dataset.csv','a',newline='') as file:
-#             writer=csv.writer(file)
-#             writer.writerows(data)
-
-# #########################################################################
-
-
-# 
