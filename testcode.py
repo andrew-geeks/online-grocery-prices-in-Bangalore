6f741385e023594d52ceb881648c82531902db08
Andrew George Issac <andrewgeorgeissac@gmail.com>
2023-08-01 11:51:54 +0530

    main

--- testcode.py
+++ testcode.py
@@ -3,12 +3,68 @@ from selenium import webdriver
 import chromedriver_autoinstaller
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-import edgedriver_autoinstaller
 from selenium.webdriver.common.by import By
 import datetime
 import json
 
-edgedriver_autoinstaller.install() 
-driver = webdriver.Edge()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
+#chromedriver_autoinstaller.install()
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(options=chrome_options,executable_path='chromedriver.exe')
+f = open('list.json')
+j_data = json.load(f)
+
+
+def getData(fname,url):
+    print(fname)
+    driver.get(url)
+    value=driver.find_element(By.CLASS_NAME, 'IyLvo')
+    value=value.text
+    value1=driver.find_element(By.CLASS_NAME, '_2ifWF')
+    value1=value1.text
+    value2 = driver.find_element(By.CLASS_NAME,'_21awm')
+    value2 = value2.find_element(By.CLASS_NAME,'IyLvo')
+    #value2=driver.find_element(By.XPATH,'//*[@id="10000159"]/tr[3]/td[2]')
+    amt_discounted_prcntge=value2.text
+    MRP=value1.split(' ')
+    MRP=MRP[1] #mrp
+
+    actualPrice=value.split(' ')
+    actualPrice=actualPrice[1] #price
+    dateval=str(datetime.date.today()) 
+    date=dateval.split('-')
+    date=date[2]+'-'+date[1]+'-'+date[0] #date
+    now = datetime.datetime.now()
+    month = now.strftime("%B") #month
+
+    #csv
+    data=[[date,month,MRP,actualPrice,amt_discounted_prcntge]]
+    with open('datasets/'+fname+'price_dataset.csv','a',newline='') as file:
+                writer=csv.writer(file)
+                writer.writerows(data)
+
+
+
+
+for i in j_data["data"]:
+    keys = i.keys()
+    values = i.values()
+    for j in keys:
+        try:
+            getData(j,i[j])
+        except:
+            pass
+        
+       
+driver.close()      
