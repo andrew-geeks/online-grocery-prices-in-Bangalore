85821a79eccb74e85c9dd5e2ec525c455f27d947
Andrew George Issac <andrewgeorgeissac@gmail.com>
2023-10-03 00:18:15 +0530

    update: scrape code

--- scrape.py
+++ scrape.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import datetime
 import json
+import time 
 
 chromedriver_autoinstaller.install()
 chrome_options = Options()
@@ -22,6 +23,7 @@ for option in options:
     chrome_options.add_argument(option)
 
 driver = webdriver.Chrome(options=chrome_options)
+#driver = webdriver.Chrome(options=chrome_options,executable_path='chromedriver.exe')
 f = open('list.json')
 j_data = json.load(f)
 
@@ -29,27 +31,32 @@ j_data = json.load(f)
 def getData(fname,url):
     print(fname)
     driver.get(url)
-    value=driver.find_element(By.CLASS_NAME, 'IyLvo')
+    time.sleep(1)
+    value=driver.find_element(By.CLASS_NAME, 'fLZywG')
     value=value.text
-    value1=driver.find_element(By.CLASS_NAME, '_2ifWF')
-    value1=value1.text
-    value2 = driver.find_element(By.CLASS_NAME,'_21awm')
-    value2 = value2.find_element(By.CLASS_NAME,'IyLvo')
-    #value2=driver.find_element(By.XPATH,'//*[@id="10000159"]/tr[3]/td[2]')
-    amt_discounted_prcntge=value2.text
-    MRP=value1.split(' ')
-    MRP=MRP[1] #mrp
-
     actualPrice=value.split(' ')
-    actualPrice=actualPrice[1] #price
+    actualPrice = actualPrice[1][1:]
+    #print("actualPrice: "+actualPrice)
+    
+    value1=driver.find_element(By.XPATH, '//*[@id="siteLayout"]/div/div[1]/section[1]/div[2]/section[1]/table/tr[1]/td[2]')
+    value1=value1.text
+    MRP=value1[1:]
+    #print("MRP:"+MRP)
+    
+    value2 = driver.find_element(By.XPATH,'//*[@id="siteLayout"]/div/div[1]/section[1]/div[2]/section[1]/table/tr[3]/td[2]').text
+    amt_discounted_prcntge=value2.split(" ")[0]
+    #print("Discount:"+amt_discounted_prcntge)
+    
+    
     dateval=str(datetime.date.today()) 
     date=dateval.split('-')
     date=date[2]+'-'+date[1]+'-'+date[0] #date
     now = datetime.datetime.now()
     month = now.strftime("%B") #month
-
+    
     #csv
     data=[[date,month,MRP,actualPrice,amt_discounted_prcntge]]
+    print(data)
     with open('datasets/'+fname+'price_dataset.csv','a',newline='') as file:
                 writer=csv.writer(file)
                 writer.writerows(data)
